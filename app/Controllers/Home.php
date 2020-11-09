d0f5836eac9cf892eeedf8b95a320fb3633c223e
David042 <Davidsarka242@gmail.com>
2020-11-08 23:32:38 -0800

    upload

---
+++ app/Controllers/Home.php
@@ -0,0 +1,24 @@
+<?php namespace App\Controllers;
+
+class Home extends BaseController
+{
+	public function help()
+	{
+		return view('welcome_message');
+	}
+
+
+	public function index()
+	{
+		return view('home');
+	}
+
+
+	public function head()
+	{
+		return view('header');
+	}
+
+	//--------------------------------------------------------------------
+
+}
